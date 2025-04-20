f720a33584094787681801b23aa3261008ee63d8
fm-labs <flowmotionlabs@gmail.com>
2025-04-20 22:24:14 +0200

    Explicitly using a MutableRefObject to fix: error TS2540: Cannot assign to 'current' because it is a read-only property.

--- packages/common/lib/helpers/useData/index.ts
+++ packages/common/lib/helpers/useData/index.ts
@@ -1,12 +1,11 @@
 import * as React from 'react'
-import { RefObject } from 'react'
 
 export function useData<T>(loader: () => Promise<T>): {
   data: T | undefined
   dataRef: React.RefObject<T | null>
 } {
   const [data, setData] = React.useState<T>()
-  const dataRef: RefObject<T | null> = React.useRef<T | null>(null)
+  const dataRef: React.MutableRefObject<T | null> = React.useRef<T | null>(null)
 
   React.useEffect(() => {
     loader().then((result) => {
