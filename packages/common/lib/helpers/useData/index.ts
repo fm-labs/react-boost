e4a349fa9428cb8c6c37650df71a82bab47f3831
fm-labs <flowmotionlabs@gmail.com>
2025-04-20 22:15:47 +0200

    Fixed mutable refs

--- packages/common/lib/helpers/useData/index.ts
+++ packages/common/lib/helpers/useData/index.ts
@@ -1,11 +1,12 @@
 import * as React from 'react'
+import { RefObject } from 'react'
 
 export function useData<T>(loader: () => Promise<T>): {
   data: T | undefined
   dataRef: React.RefObject<T | null>
 } {
   const [data, setData] = React.useState<T>()
-  const dataRef = React.useRef<T | null>(null)
+  const dataRef: RefObject<T | null> = React.useRef<T | null>(null)
 
   React.useEffect(() => {
     loader().then((result) => {
